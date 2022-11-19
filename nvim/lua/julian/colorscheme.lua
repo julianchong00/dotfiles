4c6dbb6833c07af1ed890ba6c103ac5fbec50bdd
Julian Chong <26028372+julianchong00@users.noreply.github.com>
2022-11-19 22:36:16 +1100

    style(colourscheme): trying out catppuccin colourscheme

--- nvim/lua/julian/colorscheme.lua
+++ nvim/lua/julian/colorscheme.lua
@@ -1,16 +1,26 @@
-local colorscheme = "tokyonight"
+-- local colorscheme = "tokyonight"
+--
+-- require(colorscheme).setup({
+--     style = "storm",
+--     transparent = false,
+--     terminal_colors = true,
+--     styles = {
+--         comments = { italic = false },
+--     },
+-- })
+
+local colorscheme = "catppuccin"
 
 require(colorscheme).setup({
-    style = "storm",
-    transparent = false,
-    terminal_colors = true,
-    styles = {
-        comments = { italic = false },
-    },
+	flavour = "frappe", -- latte, frappe, macchiato, mocha
+	term_colors = true,
+	styles = {
+		comments = {},
+	},
 })
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
-    return
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
 end
